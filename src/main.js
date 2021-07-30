14d3f4de3ccf72c2f724b14bc0aceecfe276177f
Mahdi Kheibari <mahdi.khaybari@gmail.com>
2021-07-31 00:04:51 +0430

    installed bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 import App from './App.vue'
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 new Vue({
   el: '#app',
